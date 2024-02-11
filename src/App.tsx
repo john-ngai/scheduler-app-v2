3b2e2471283ef8a37ae0fd704ee122aecb6241ab
mrjohnming <john.ngai@hotmail.com>
2024-02-10 20:04:18 -0500

    Lightly refactor the App component

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
+export const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -27,5 +27,3 @@ function App() {
     </div>
   )
 }
-
-export default App
