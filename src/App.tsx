ffe67221e8f5e2ebab6bc357e0cdc8df14b2d96a
mrjohnming <john.ngai@hotmail.com>
2024-02-11 09:07:00 -0500

    Add a form template with a drop down select menu

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,18 @@
  * All Rights Reserved
  */
 
-import { Box, Paper, type SxProps, type Theme } from '@mui/material'
-import React from 'react'
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Select,
+  type SelectChangeEvent,
+  type SxProps,
+  type Theme,
+} from '@mui/material'
+import React, { useCallback, useState } from 'react'
 
 const styles: Record<string, SxProps<Theme>> = {
   box: {
@@ -17,13 +27,42 @@ const styles: Record<string, SxProps<Theme>> = {
     width: '50vw',
     height: '50vh',
     marginTop: 10,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  formControl: {
+    width: 200,
+    marginTop: 5,
   },
 }
 
-export const App: React.FC = () => (
-  <Box sx={styles.box}>
-    <Paper sx={styles.paper} elevation={4}>
-      Hello world
-    </Paper>
-  </Box>
-)
+export const App: React.FC = () => {
+  const [value, setValue] = useState<string>('')
+
+  const onChange = useCallback(
+    (event: SelectChangeEvent) => setValue(event.target.value),
+    []
+  )
+
+  return (
+    <Box sx={styles.box}>
+      <Paper sx={styles.paper} elevation={4}>
+        <FormControl sx={styles.formControl}>
+          <InputLabel id="demo-simple-select-label">Age</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={value}
+            label="Age"
+            onChange={onChange}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+      </Paper>
+    </Box>
+  )
+}
