5da0bc0de393619f9ddfe920a0b69f713ac97dda
mrjohnming <john.ngai@hotmail.com>
2024-02-10 20:10:16 -0500

    Import robot fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
+/**
+ * Copyright (c) 2024 John Ngai
+ * All Rights Reserved
+ */
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { App } from './App'
 import reportWebVitals from './reportWebVitals'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
