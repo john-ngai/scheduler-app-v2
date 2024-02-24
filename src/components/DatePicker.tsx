444a70b252d43ed8ecb88347c5c7abccc1a03a34
mrjohnming <john.ngai@hotmail.com>
2024-02-23 19:05:58 -0500

    Setup the initial controlled DatePicker state

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -5,16 +5,43 @@
 
 import { DesktopDatePicker, LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3'
+import { getDate, getMonth, getYear } from 'date-fns'
 import { enUS } from 'date-fns/locale/en-US'
-import { useMemo } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 
 export const DatePicker: React.FC = () => {
-  const defaultValue = useMemo(() => Date.now(), [])
+  const initialDate = useMemo(() => {
+    const dateNow = Date.now()
+    const year = getDate(dateNow)
+    const month = getMonth(dateNow) + 1
+    const day = getYear(dateNow)
+    return { dateNow, year, month, day }
+  }, [])
+
+  const [date, setDate] = useState(initialDate)
+
+  const onDayChange = useCallback((value: number | null) => {
+    const day = getDate(value as number)
+    setDate((prev) => ({ ...prev, day }))
+  }, [])
+
+  const onMonthChange = useCallback((value: number) => {
+    const month = getMonth(value) + 1
+    setDate((prev) => ({ ...prev, month }))
+  }, [])
+
+  const onYearChange = useCallback((value: number) => {
+    const year = getYear(value)
+    setDate((prev) => ({ ...prev, year }))
+  }, [])
+
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={enUS}>
       <DesktopDatePicker
-        defaultValue={defaultValue}
-        onChange={(value) => console.log('date =', typeof value)}
+        value={date.dateNow}
+        onChange={onDayChange}
+        onMonthChange={onMonthChange}
+        onYearChange={onYearChange}
       />
     </LocalizationProvider>
   )
